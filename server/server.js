6db767854f3e78d13de9bca3ffaf736e841abf4d
Adeel Farooq Khan <adeelfarooq417@gmail.com>
2025-07-04 22:46:53 +0500

    register user login user funtionality

--- server/server.js
+++ server/server.js
@@ -1,8 +1,23 @@
-import express from 'express'
-import "dotenv/config"
-import cors from 'cors'
+import express from "express";
+import "dotenv/config";
+import cors from "cors";
+import connectDB from "./configs/db.js";
+import userRouter from "./routes/userRoutes.js";
 
+// initialize express app
 
-// initialize express app 
+const app = express();
 
+await connectDB();
 
+//Middlewares
+app.use(cors());
+app.use(express.json());
+
+app.get("/", (req, res) => res.send("Server is Running"));
+
+app.use("/api/user", userRouter);
+
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, () => console.log(`Server is running on port ${PORT}`));
